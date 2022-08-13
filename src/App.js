743d432163aa6471e5a25042c2b05bc9aa406228
Damian Padilla <damipad4@gmail.com>
2022-08-13 11:20:21 -0500

    Adding changes from PC

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
 
         return (
             <div className="container">
-                <Table characters={characters}/>
+                <Table characterDate={characters}/>
             </div>
         )
     }
